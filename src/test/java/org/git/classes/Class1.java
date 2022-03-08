8e24c121d5d1f3c7f48c865af63e5818ac132e86
Andrewvimal56 <andrewvimal56@gmail.com>
2022-03-08 18:21:25 +0530

    AdactinLogin

---
+++ src/test/java/org/git/classes/Class1.java
@@ -0,0 +1,16 @@
+package org.git.classes;
+
+public class Class1 {
+	public void method1() {
+		System.out.println("One");
+	}
+
+	public void method2() {
+		System.out.println("Two");
+	}
+
+	public void method3() {
+		System.out.println("Three");
+	}
+
+}
