8392b80818ad09e78e9d9cb22917b86b057fc26f
Kapilnagarajan <kapilnagarajan@gmail.com>
2022-03-10 10:57:51 +0530

    Kapil Code

--- src/test/java/org/git/classes/Class1.java
+++ src/test/java/org/git/classes/Class1.java
@@ -12,11 +12,26 @@ public class Class1 {
 	public void method3() {
 		System.out.println("Three");
 	}
+
 	public void method4() {
 		System.out.println("Three");
 	}
+
 	public void method5() {
 		System.out.println("Three");
+	}
+
+	public void method6() {
+		System.out.println("Six");
+	}
+
+	public void method7() {
+		System.out.println("Seven");
+	}
+
+	public void method8() {
+		System.out.println("Eight");
+
 	}
 
 }
